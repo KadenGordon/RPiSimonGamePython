7da067bd82eed97a930eddc017e5ce8d9f8fdad0
KadenGordon <kaden.m.gordon@gmail.com>
2017-11-05 18:44:26 -0700

    I think this will work for now

--- Simon.py
+++ Simon.py
@@ -3,7 +3,6 @@ from GPIO import LED, Button #my own GPIO device classes
 from SimonClass import SimonGame
 import RPi.GPIO as GPIO
 
-
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 #creates game object with these pins
@@ -17,51 +16,45 @@ game = SimonGame([3, 5, 8, 10], [11, 13, 16, 18])
 
 game.turnOffLEDs()
 #stuff to start the game
-class fail ( exception ):
-	pass
 
 game.start()
 wait(1)
-def playgame():
-		while True:
-			position = 0
-			game.createLevel()
-			game.flashPattern(game.pattern)
-			#play level
-			i = 0
-			while position < len(game.pattern):
-				goal = game.pattern[position]
-				keys = game.getKeys()
-				#check if it's the right one, if it is, move on to next position, else game over.
-				buttonpos = i%4
-				button = game.Button[buttonpos]
-				if button.isPressed():
-					game.LED[buttonpos].turnOn()
-					if game.Button[goal].isPressed():
-						position += 1
-						print "did a thing"
-
-					else: 
-						game.gameOver()
-						print "fail " + str(button)
-						break break
-					button.waitUntilNotPressed()
-				else:
-					game.LED[buttonpos].turnOff()
-				i += 1
-			print "LEVEL COMPLETE"
-			game.level += 1
-			game.turnOffLEDs()
-			wait(1)
 #loop
 try:
 	#forever
 	while True:
-		playgame()
+		position = 0
+		game.createLevel()
+		game.flashPattern(game.pattern)
+		#play level
+		i = 0
+		while position < len(game.pattern):
+			goal = game.pattern[position]
+			keys = game.getKeys()
+			#check if it's the right one, if it is, move on to next position, else game over.
+			buttonpos = i%4
+			button = game.Button[buttonpos]
+			if button.isPressed():
+				game.LED[buttonpos].turnOn()
+				if game.Button[goal].isPressed():
+					position += 1
+					print "did a thing"
+
+				else: 
+					game.gameOver()
+					print "fail " + str(button)
+					raise SystemExit
+					#needs to start the game from the beginning somehow...
+				button.waitUntilNotPressed()
+			else:
+				game.LED[buttonpos].turnOff()
+			i += 1
+		print "LEVEL COMPLETE"
+		game.level += 1
+		game.turnOffLEDs()
+		wait(1)
 
 except KeyboardInterrupt:
 	GPIO.cleanup()
 except SystemExit:
 	GPIO.cleanup()
-except fail:
-	playgame()
